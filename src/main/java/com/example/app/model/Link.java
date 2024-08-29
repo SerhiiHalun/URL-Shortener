c9e715d38147f924a2a901e64100bbba5829515a
oldiright <oliynuk_Dima@ukr.net>
2024-08-29 21:15:28 +0300

    added dbH2, migrations, and entities;

--- src/main/java/com/example/app/model/Link.java
+++ src/main/java/com/example/app/model/Link.java
@@ -1,4 +1,60 @@
 package com.example.app.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "link")
+
 public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "seq_link_id", allocationSize = 1)
+    private long id;
+
+    @ToString.Exclude
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "full_url")
+    private String fullUrl;
+
+    @Column(name = "short_url")
+    private String shortUrl;
+
+    @Column(name = "creation_date")
+    private LocalDate creationDate;
+
+    @Column(name = "order_status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @Column(name = "transition_counter")
+    private int transitionCounter;
+
+    public enum OrderStatus {
+        ACTIVE,
+        DISABLE
+    }
 }
---
+++ src/main/resources/db/migration/V2__populate.sql
@@ -0,0 +1,12 @@
+INSERT INTO users (username, password)
+VALUES ('Jon Yon', 'jRRfvknfvkf888vl'),
+       ('Lisa Dou', 'rrrfrR78787RRR'),
+       ('Erik Lehnsherr', 'B4r4rlflrllj');
+
+INSERT INTO link (full_url, short_url, order_status, user_id, transition_counter)
+VALUES ('https://www.baeldung.com/spring-boot-h2-database', '7ppk09', 'ACTIVE', 1, 0),
+       ('https://mvnrepository.com/artifact/com.h2database/h2/2.2.224', 'ewq12e4e', 'ACTIVE', 2, 0),
+       ('https://jobs.dou.ua/vacancies/?category=Java', 'tgb7yhf', 'ACTIVE', 3, 0),
+       ('https://github.com/Prosperro-de/Dev17Final/tree/main/src/main', 'ghfb766', 'ACTIVE', 1, 0),
+       ('https://www.h2database.com/html/systemtables.html?highlight=DEFAULT_ON_NULL&search=default%20#information_schema_schemata', 'uhhtu7r4', 'ACTIVE', 1, 0),
+       ('https://www.oracle.com/developer/devlive/', 'cvbt6ju8', 'ACTIVE', 3, 0);
