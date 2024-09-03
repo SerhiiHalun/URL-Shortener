6e563078c39c23fcbe61e80c43b6a1a9c0cd7a38
Roman horodyskyi <gorod10516@gmail.com>
2024-09-03 22:13:45 +0300

    Add security, and configure JWT.

---
+++ src/main/java/com/example/app/controller/AuthController.java
@@ -0,0 +1,39 @@
+package com.example.app.controller;
+
+import com.example.app.model.User;
+import lombok.RequiredArgsConstructor;
+import com.example.app.model.dto.AuthRequest;
+import com.example.app.model.dto.SignupRequest;
+import com.example.app.service.JwtUtil;
+import com.example.app.service.UserService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private  final JwtUtil jwtUtil;
+
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody SignupRequest signupRequest) {
+        return userService.createUser(signupRequest);
+    }
+
+    @PostMapping("/auth")
+    public String createAuthenticationToken(@RequestBody AuthRequest authenticationRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+        );
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        return jwtUtil.generateToken(userDetails);
+    }
+}
